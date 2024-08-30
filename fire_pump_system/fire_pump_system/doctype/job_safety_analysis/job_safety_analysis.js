175674ad366a15188850866a1b929fea73a47bf4
Steven Kilvington <freefallkilvo@yahoo.com.au>
2024-08-30 15:50:56 +1000

    Update some automations for JSA Functions and Maps

---
+++ fire_pump_system/fire_pump_system/doctype/job_safety_analysis/job_safety_analysis.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Fire Pump Systems and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Job Safety Analysis", {
+// 	refresh(frm) {
+
+// 	},
+// });
