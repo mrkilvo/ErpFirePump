ff184c36442a36c39f2dd1aa29c2ce6c2102d5f7
Steven Kilvington <freefallkilvo@yahoo.com.au>
2024-09-18 18:37:14 +1000

    Modified some automations and customisations for Sales Order forms

---
+++ fire_pump_system/fire_pump_system/doctype/as1851_2012_pumps_annual/as1851_2012_pumps_annual.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Fire Pump Systems and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("AS1851-2012-Pumps Annual", {
+// 	refresh(frm) {
+
+// 	},
+// });
